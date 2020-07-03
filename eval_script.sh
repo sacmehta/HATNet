fb7b0bd941211aa36a9e18255845363bb8d150dd
sacmehta <sacmehta@uw.edu>
2020-07-03 16:15:05 -0700

    TOC

--- eval_script.sh
+++ eval_script.sh
@@ -6,4 +6,4 @@ config_file='model_zoo/espnetv2_config_bag_1792_word_256_softmax_l2.json' # path
 test_file='./wsi_dataset/splits/test.txt' # name of the file that contains image and class labels
 
 # run the evaluation code
-CUDA_VISIBLE_DEVICES=0 python main_evaluation.py --img-dir $img_dir --weights-test $weights_file --config-file $config_file --test-file $4
+CUDA_VISIBLE_DEVICES=0 python main_evaluation.py --img-dir $img_dir --weights-test $weights_file --config-file $config_file --test-file $test_file
